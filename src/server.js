c567977e255803e8495ff128602e91f7b5556853
buzzler0109 <144600227+buzzler0109@users.noreply.github.com>
2024-12-18 21:42:44 +0300

    fix

--- src/server.js
+++ src/server.js
@@ -36,19 +36,23 @@ export const setupServer = () => {
     });
   });
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found!',
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    try {
+      const { contactId } = req.params;
+      const contact = await getContactById(contactId);
+      if (!contact) {
+        res.status(404).json({
+          message: 'Contact not found!',
+        });
+      }
+      res.status(200).json({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
       });
+    } catch (error) {
+      next(error);
     }
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
   });
 
   app.use('*', (req, res, next) => {

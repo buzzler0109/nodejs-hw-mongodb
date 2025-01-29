17648b89235f477a25f58a775acfc4c18c80d1af
buzzler0109 <144600227+buzzler0109@users.noreply.github.com>
2025-01-30 00:04:26 +0300

    change

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -6,10 +6,7 @@ import { SWAGGER_PATH } from '../constants/index.js';
 export const swaggerDocs = () => {
   try {
     const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
-    return {
-      serve: swaggerUI.serve,
-      setup: swaggerUI.setup(swaggerDoc),
-    };
+    return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
   } catch (err) {
     return (req, res, next) =>
       next(createHttpError(500, "Can't load swagger docs"));
